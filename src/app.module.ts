6baf2b589db52a3f248db96dfe6b426f6fae03cb
wolfsoul <alejandrof011021@gmail.com>
2023-11-06 13:59:00 -0500

    Install and add the proyect mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BooksModule } from './books/books.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [BooksModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/superLetterDB'),
+    BooksModule,
+  ],
 })
 export class AppModule {}
